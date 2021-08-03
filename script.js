69f98a10c40c9418447e96aec8c9c9ea60733f38
Mamadou Diallo <dioums.diallo@gmail.com>
2021-08-03 18:22:49 +0000

    final touches

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ function compute()
     var principal = document.getElementById("principal").value;
     if (principal <= 0) {
         alert("Enter a positive number");
-        principal.focus();
-        //return false
+        fprincipal.focus();
+        return false
     }
-    //return true;
+    return true;
 
     var rate = document.getElementById("rate").value;
 
@@ -17,10 +17,10 @@ function compute()
     var year = new Date().getFullYear() + parseInt(years);
 
     document.getElementById("result").innerHTML = 
-    `<p>If you deposit ${principal},</p>
-    <p>at an interest rate of ${rate}%.</p>
-    <p>You will receive an amount of ${interest},</p>
-    <p>in the year ${year}.</p>`;
+    `<p>If you deposit <span class="highlight">${principal}</span>,</p>
+    <p>at an interest rate of <span class="highlight">${rate}%</span>.</p>
+    <p>You will receive an amount of <span class="highlight">${interest}</span>,</p>
+    <p>in the year <span class="highlight">${year}</span>.</p>`;
     
 }
 function updateRate()
