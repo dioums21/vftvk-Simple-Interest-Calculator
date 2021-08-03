0ec9d05e4915e59e0aca058014eb6d9c19af4177
Mamadou Diallo <dioums.diallo@gmail.com>
2021-08-03 03:09:34 +0000

    changes made to html, css, and js files

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var principal = document.getElementById("principal").value;
+
+    var rate = document.getElementById("rate").value;
+
+    var years = document.getElementById("years").value;
+
+    var compute = principal * (rate/100) * years;
+
+    document.getElementById("result").innerHTML = 
+    `<p>If you depodit ${principal},</p>
+    <p>at an interest rate of ${rate}%.</p>
+    <p>You will receive an amount of ${compute}</p>,
+    <p>in the year ${2020 + years}</p>`;
     
 }
         
