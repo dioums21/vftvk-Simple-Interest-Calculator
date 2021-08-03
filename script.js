cc98a50c06d6b62bec287d453c7e0782f918d899
Mamadou Diallo <dioums.diallo@gmail.com>
2021-08-03 04:03:18 +0000

    changes made to html, css, and js files No. 2

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
+function updateRate()
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval;
+}
+
 function compute()
 {
     var principal = document.getElementById("principal").value;
+    if (principal <= 0) {
+        alert("Enter a positive number");
+        fprincipal.focus();
+        return false
+    }
+    return true;
 
     var rate = document.getElementById("rate").value;
 
     var years = document.getElementById("years").value;
 
-    var compute = principal * (rate/100) * years;
+    var interest = principal * (rate/100) * years;
+
+    var year = new Date().getFullYear() + parseInt(years);
 
     document.getElementById("result").innerHTML = 
     `<p>If you depodit ${principal},</p>
     <p>at an interest rate of ${rate}%.</p>
-    <p>You will receive an amount of ${compute}</p>,
-    <p>in the year ${2020 + years}</p>`;
+    <p>You will receive an amount of ${interest},</p>
+    <p>in the year ${year}.</p>`;
     
 }
         
